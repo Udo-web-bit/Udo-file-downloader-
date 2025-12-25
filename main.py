1499bb2c6d4ffdab6b5ea3d01bf55f9e1f36a5c9
Udo-web-bit <callmeblessedudomisor@gmail.com>
2025-12-26 00:17:31 +0100

    Fix syntax error in uvicorn run command

--- main.py
+++ main.py
@@ -307,4 +307,4 @@ async def webhook(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000")))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
